cd2c111f28ec85049a7a2baf3a51aacad425f0ad
Luan Jesus <luanpjesus@gmail.com>
2020-10-03 13:33:53 -0300

    languages updates

--- snakeGit2.py
+++ snakeGit2.py
@@ -8,17 +8,17 @@ import random
 delay = 0.1
 
 # Score
-score = 0
-high_score = 0
+Pontuacao = 0
+Pontuacao_maxima = 0
 
-# Set up the screen
+# Set up da tela
 wn = turtle.Screen()
 wn.title("Snake Game by Luan Jesus")
 wn.bgcolor("purple")
 wn.setup(width=600, height=600)
-wn.tracer(0) # Turns off the screen updates
+wn.tracer(0) 
 
-# Snake head
+# Snakehead
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
@@ -37,7 +37,7 @@ food.goto(0,100)
 
 segments = []
 
-# Pen
+
 pen = turtle.Turtle()
 pen.speed(0)
 pen.shape("square")
@@ -45,7 +45,7 @@ pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+pen.write("Pontuacao:0  Pontuacao maxima:0", align="center", font=("Courier", 22, "normal"))
 
 # Functions
 def go_up():
@@ -92,37 +92,37 @@ wn.onkeypress(go_right, "d")
 while True:
     wn.update()
 
-    # Check for a collision with the border
+    
     if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
         time.sleep(1)
         head.goto(0,0)
         head.direction = "stop"
 
-        # Hide the segments
+        
         for segment in segments:
             segment.goto(1000, 1000)
         
-        # Clear the segments list
+      
         segments.clear()
 
-        # Reset the score
-        score = 0
+       
+        Pontuacao = 0
 
-        # Reset the delay
+        
         delay = 0.1
 
         pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+        pen.write("Pontuacao: {}  Pontuacao maxima: {}".format(Pontuacao, Pontuacao_maxima), align="center", font=("Courier", 22, "normal")) 
 
 
-    # Check for a collision with the food
+    
     if head.distance(food) < 20:
-        # Move the food to a random spot
+        
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
         food.goto(x,y)
 
-        # Add a segment
+        
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape("square")
@@ -130,25 +130,25 @@ while True:
         new_segment.penup()
         segments.append(new_segment)
 
-        # Shorten the delay
+       
         delay -= 0.001
 
-        # Increase the score
-        score += 10
+       
+        Pontuacao += 10
 
-        if score > high_score:
-            high_score = score
+        if Pontuacao > Pontuacao_maxima:
+            Pontuacao_maxima = Pontuacao
         
         pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+        pen.write("Pontuacao: {}  Pontuacao maxima: {}".format(Pontuacao, Pontuacao_maxima), align="center", font=("Courier", 22, "normal")) 
 
-    # Move the end segments first in reverse order
+    
     for index in range(len(segments)-1, 0, -1):
         x = segments[index-1].xcor()
         y = segments[index-1].ycor()
         segments[index].goto(x, y)
 
-    # Move segment 0 to where the head is
+   
     if len(segments) > 0:
         x = head.xcor()
         y = head.ycor()
@@ -156,29 +156,29 @@ while True:
 
     move()    
 
-    # Check for head collision with the body segments
+    
     for segment in segments:
         if segment.distance(head) < 20:
             time.sleep(1)
             head.goto(0,0)
             head.direction = "stop"
         
-            # Hide the segments
+            
             for segment in segments:
                 segment.goto(1000, 1000)
         
-            # Clear the segments list
+            
             segments.clear()
 
-            # Reset the score
-            score = 0
+           
+            Pontuacao = 0
 
-            # Reset the delay
+            
             delay = 0.1
         
-            # Update the score display
+            
             pen.clear()
-            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+            pen.write("Pontuacao: {}  Pontuacao maxima: {}".format(Pontuacao, Pontuacao_maxima), align="center", font=("Couriaer", 22, "normal"))
 
     time.sleep(delay)
 
