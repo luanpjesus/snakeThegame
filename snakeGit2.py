8006a4e7b78e2a4a5dff692789dd94a1480e3a0a
Luan Jesus <luanpjesus@gmail.com>
2020-10-03 12:31:45 -0300

    new uptade (°-°)

--- snakeGit2.py
+++ snakeGit2.py
@@ -1,5 +1,5 @@
-# Simple Snake Game in Python 3 for Beginners
-# By @TokyoEdTech
+# Simple Snake Game in Python 3 
+
 
 import turtle
 import time
@@ -13,7 +13,7 @@ high_score = 0
 
 # Set up the screen
 wn = turtle.Screen()
-wn.title("Snake Game by @TokyoEdTech")
+wn.title("Snake Game by Luan Jesus")
 wn.bgcolor("purple")
 wn.setup(width=600, height=600)
 wn.tracer(0) # Turns off the screen updates
